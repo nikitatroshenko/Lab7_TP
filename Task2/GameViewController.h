ef7d49cef7305c73c36f071a1d045754616b387a
nikitatroshenko <nikitatroshenko@gmail.com>
2016-05-11 12:41:02 +0300

    Add files via upload

---
+++ Task2/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  TPLab7.2
+//
+//  Created by fpmi on 11.05.16.
+//  Copyright (c) 2016 Nikson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
